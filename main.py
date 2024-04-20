8ebd6a76aea52caef8a23cfc8e42ee096950500a
Duarte Laia <duarte.laia04@gmail.com>
2024-04-20 15:28:26 +0100

    base

---
+++ main.py
@@ -0,0 +1,69 @@
+from sys import stdin
+
+'''
+Esta classe representa os estados utilizados nos algoritmos de procura.
+'''
+class PipeManiaState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = PipeManiaState.state_id
+        PipeManiaState.state_id += 1
+
+    def __lt__(self, other):
+        """ Este método é utilizado em caso de empate na gestão da lista
+        de abertos nas procuras inf """
+
+
+'''
+Representação interna de uma grelha de PipeMania.
+'''
+class Board:
+    
+    def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
+        """ Devolve os valores imediatamente acima e abaixo,
+        respectivamente. """
+        pass
+    def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
+        """ Devolve os valores imediatamente à esquerda e à direita,
+        respectivamente. """
+        pass
+
+
+class PipeMania(Problem):
+    def __init__(self, initial_state: Board, goal_state: Board):
+        """ O construtor especifica o estado inicial. """
+        pass
+
+    '''
+    Retorna uma lista de ações que podem ser executadas a
+    partir do estado passado como argumento.
+    '''
+    def actions(self, state: State):
+        pass
+
+    '''
+    Retorna o estado resultante de executar a 'action' sobre
+    'state' passado como argumento. A ação a executar deve ser uma
+    das presentes na lista obtida pela execução de
+    self.actions(state). 
+    '''
+    def result(self, state: State, action):
+        pass
+
+    '''
+    Função heuristica utilizada para a procura A*.
+    '''
+    def h(self, node: Node):
+        pass
+
+
+'''
+Lê a instância do problema do standard input (stdin)
+e retorna uma instância da classe Board.
+'''
+@staticmethod
+def parse_instance():
+    # ler input    
+    pass
